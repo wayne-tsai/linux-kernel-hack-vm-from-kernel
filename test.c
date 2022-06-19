99884ecbdf768406f20ed0854f866c79e3bde18f
Walter <walter@localhost.localdomain>
2022-06-19 05:52:38 -0700

    work, still refactoring

--- test.c
+++ test.c
@@ -4,20 +4,14 @@
 #include <unistd.h>
 int main()
 {
-  int a=8;
+  int a=5;
   int b=6;
   int c=7;
   int d=8;
-  int counter=0;
-  void* p=&a;
   printf("%d\n",getpid());
   while(1){
-    printf("Counter: %d\n", ++counter);
-    printf("Value: %d\n", a);
-    printf("Address: %p\n", &a);
-    printf("Addr p point to: %p\n", p);
-    printf("Value p point to: %d\n", *(int*)p);	
-    sleep(5);
+    printf("%d\n",a);
+    sleep(1);
   }
-    return 0;
+  return 0;
 }
